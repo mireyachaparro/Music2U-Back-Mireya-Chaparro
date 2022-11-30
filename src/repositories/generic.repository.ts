7303efe5dcaa15b728bcf9223f46c533da794bff
mireyachaparro <mireyacn99@gmail.com>
2022-11-30 18:58:35 +0100

    Update generic album repository

--- src/repositories/generic.repository.ts
+++ src/repositories/generic.repository.ts
@@ -1,3 +1,4 @@
+import { Album } from '../entities/album.entity';
 import { User } from '../entities/user.entity';
 
 export type id = number | string;
@@ -12,10 +13,10 @@ export interface UserRepoGeneric {
 }
 
 export interface AlbumRepoGeneric {
-    getAll: () => Promise<Array<User>>;
-    get: (id: id) => Promise<User>;
-    post: (data: Partial<User>) => Promise<User>;
-    find: (data: Partial<User>) => Promise<User>;
-    patch: (id: id, data: Partial<User>) => Promise<User>;
+    getAll: () => Promise<Array<Album>>;
+    get: (id: id) => Promise<Album>;
+    post: (data: Partial<Album>) => Promise<Album>;
+    find: (data: Partial<Album>) => Promise<Album>;
+    patch: (id: id, data: Partial<Album>) => Promise<Album>;
     delete: (id: id) => Promise<void>;
 }
