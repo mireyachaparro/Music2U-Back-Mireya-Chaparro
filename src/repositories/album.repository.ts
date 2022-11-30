af45cc7c98475086db9948809487c5feaad09a7c
mireyachaparro <mireyacn99@gmail.com>
2022-11-30 19:38:06 +0100

    Add album repository

---
+++ src/repositories/album.repository.ts
@@ -0,0 +1,66 @@
+import { Types } from 'mongoose';
+import { Album, AlbumModel, ProtoAlbum } from '../entities/album.entity';
+import { AlbumRepoGeneric, id } from './generic.repository';
+
+export class AlbumRepository implements AlbumRepoGeneric {
+    static instance: AlbumRepository;
+
+    public static getInstance(): AlbumRepository {
+        if (!AlbumRepository.instance) {
+            AlbumRepository.instance = new AlbumRepository();
+        }
+        return AlbumRepository.instance;
+    }
+
+    #Model = AlbumModel;
+
+    async getAll(): Promise<Array<Album>> {
+        const result = this.#Model.find().populate('owner', {
+            albums: 0,
+        });
+        return result;
+    }
+
+    async get(id: id): Promise<Album> {
+        const result = await this.#Model
+            .findById(id)
+            .populate<{ _id: Types.ObjectId }>('owner');
+        if (!result) throw new Error('ID not found');
+        return result;
+    }
+
+    async find(search: Partial<Album>): Promise<Album> {
+        const result = await this.#Model.findOne(search).populate('owner', {
+            albums: 0,
+        });
+        if (!result) throw new Error('ID not found');
+        return result;
+    }
+
+    async post(data: ProtoAlbum): Promise<Album> {
+        const result = await (
+            await this.#Model.create(data)
+        ).populate('owner', { albums: 0 });
+        return result;
+    }
+
+    async patch(id: id, data: Partial<Album>): Promise<Album> {
+        const result = await this.#Model
+            .findByIdAndUpdate(id, data, {
+                new: true,
+            })
+            .populate('owner', {
+                albums: 0,
+            });
+        if (!result) throw new Error('ID not found');
+        return result;
+    }
+
+    async delete(id: id): Promise<void> {
+        const result = await this.#Model.findByIdAndDelete('owner', {
+            albums: 0,
+        });
+        if (!result) throw new Error('ID not found');
+        return;
+    }
+}
