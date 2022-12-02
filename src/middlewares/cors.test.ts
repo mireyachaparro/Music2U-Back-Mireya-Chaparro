5dc4f57d8c5421862097c5cda5cc2d6ab58027bd
mireyachaparro <mireyacn99@gmail.com>
2022-12-02 20:15:04 +0100

    Add cors test

---
+++ src/middlewares/cors.test.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from 'express';
+import { setCors } from './cors';
+
+describe('given cors middleware', () => {
+    const res: Partial<Response> = {
+        setHeader: jest.fn(),
+    };
+    const next: NextFunction = jest.fn();
+    describe('when origin is "*"', () => {
+        const req: Partial<Request> = {
+            header: jest.fn().mockReturnValue('*'),
+        };
+        test('then it set * header', () => {
+            setCors(req as Request, res as Response, next);
+            expect(res.setHeader).toHaveBeenCalledWith(
+                'Access-Control-Allow-Origin',
+                '*'
+            );
+        });
+    });
+
+    describe('when origin is "Origin"', () => {
+        const req: Partial<Request> = {
+            header: jest.fn().mockReturnValue('Origin'),
+        };
+        test('then it set * header', () => {
+            setCors(req as Request, res as Response, next);
+            expect(res.setHeader).toHaveBeenCalledWith(
+                'Access-Control-Allow-Origin',
+                'Origin'
+            );
+        });
+    });
+});
